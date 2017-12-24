6b09a393f95f8087e5d78c47646f153aed27a3f1
mahmoud elgeiziry <mahmoud.geiziry@hotmail.com>
2017-12-24 14:37:53 +0300

    trying to test com object comm in c++

---
+++ testConnectToProsper/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <ActiveQt/QAxBase>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+//    QAxBase::setControl("PROSPER");
+
+
+    return a.exec();
+}
